1dcea5d165a31deeed5294e537bf83247838093f
Laura McCluskey <laura.mccluskey@wales.nhs.uk>
2019-07-03 12:01:12 +0100

    tests updated

--- test/test_triplets_repeats.py
+++ test/test_triplets_repeats.py
@@ -10,10 +10,10 @@ class test_triplet_repeats(unittest.TestCase):
         self.assertEqual(len(triplets_table),7)
         self.assertEqual(len(triplets),12)
         self.assertEqual(triplets_table.iloc[0,3],406.0)        
-        self.assertEqual(triplets_table.iloc[4,4],258.9)
+        self.assertEqual(triplets_table.iloc[4,4],288.9)
 
 
-    #def test_match_control_samples_with_references(self):
+    def test_match_control_samples_with_references(self):
         triplets,triplets_table=get_triplets_table("gene1","tester1")
         controls,continue_program=match_control_samples_with_references(triplets, "gene1")
         self.assertEqual(continue_program, "yes")
@@ -21,20 +21,42 @@ class test_triplet_repeats(unittest.TestCase):
         self.assertEqual(controls.iloc[0,4],'51')
         self.assertEqual(controls.iloc[3,4],'29')
 
-    #def test_find_closest_control_peak_to_sample_peaks(self):
+
+    def test_find_closest_control_peak_to_sample_peaks(self):
+        triplets,triplets_table=get_triplets_table("gene1","tester1")
+        controls,continue_program=match_control_samples_with_references(triplets, "gene1")
+        triplets_table=find_closest_control_peak_to_sample_peaks(triplets_table,controls)
+        self.assertEqual(triplets_table.iloc[0,7],276)
+        self.assertEqual(triplets_table.iloc[0,10],'25')
+        self.assertEqual(triplets_table.iloc[3,5],376)
+        self.assertEqual(triplets_table.iloc[3,8],'51')
+        self.assertEqual(triplets_table.iloc[6,5],323)
+        self.assertEqual(triplets_table.iloc[6,8],'45')
 
 
+    def test_get_number_of_triplet_repeats(self):
+        triplets,triplets_table=get_triplets_table("gene1","tester1")
+        controls,continue_program=match_control_samples_with_references(triplets, "gene1")
+        triplets_table=find_closest_control_peak_to_sample_peaks(triplets_table,controls)
+        triplets_table=get_number_of_triplet_repeats(triplets_table)
+        self.assertEqual(triplets_table.iloc[0,12],61)
+        self.assertEqual(triplets_table.iloc[0,13],21)
+        self.assertEqual(triplets_table.iloc[1,12],54)
+        self.assertEqual(triplets_table.iloc[4,13],30)
+        self.assertEqual(triplets_table.iloc[6,11],42)
 
-    #def test_get_number_of_triplet_repeats(self):
 
-
-
-    #def test_format_columns(self):
-
-
-
-
-
+    def test_format_columns(self):
+        triplets,triplets_table=get_triplets_table("gene1","tester1")
+        controls,continue_program=match_control_samples_with_references(triplets, "gene1")
+        triplets_table=find_closest_control_peak_to_sample_peaks(triplets_table,controls)
+        triplets_table=get_number_of_triplet_repeats(triplets_table)
+        triplets_table=format_columns(triplets_table, controls, "tester1", "gene1")
+        self.assertEqual(triplets_table.iloc[0,2],406)
+        self.assertEqual(triplets_table.iloc[0,5],61)
+        self.assertEqual(triplets_table.iloc[4,4],30)
+        self.assertEqual(triplets_table.iloc[6,1],315)
+        self.assertEqual(triplets_table.iloc[6,5],55)
 
 
 if __name__=='__main__':
